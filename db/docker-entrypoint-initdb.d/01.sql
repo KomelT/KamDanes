0309387f37738d989e03056908e17802990f0058
Tit <tit.sober33@gmail.com>
2024-12-06 19:53:39 +0100

    fixed constraints

--- db/docker-entrypoint-initdb.d/01.sql
+++ db/docker-entrypoint-initdb.d/01.sql
@@ -23,34 +23,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `event` (
   `id_event` int(11) NOT NULL,
   `id_user` int(11) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `organisation` varchar(255) NOT NULL,
-  `artist_name` varchar(255) NOT NULL,
-  `date_from` date NOT NULL,
-  `date_to` date NOT NULL,
-  `loc_x` double NOT NULL,
-  `loc_y` double NOT NULL,
-  `time` time NOT NULL,
-  `age_lim` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `price` decimal(10,0) NOT NULL,
-  `type` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `organisation` varchar(255) DEFAULT NULL,
+  `artist_name` varchar(255) DEFAULT NULL,
+  `date_from` date DEFAULT NULL,
+  `date_to` date DEFAULT NULL,
+  `loc_x` double DEFAULT NULL,
+  `loc_y` double DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `age_lim` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
   `link` text DEFAULT NULL,
   `online` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_slovenian_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_slovenian_ci;
 
 --
@@ -62,13 +48,8 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `event`
   ADD PRIMARY KEY (`id_event`),
+  ADD UNIQUE KEY `link` (`link`) USING HASH,
   ADD KEY `id_user` (`id_user`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Constraints for dumped tables
@@ -80,7 +61,3 @@ ALTER TABLE `users`
 ALTER TABLE `event`
   ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;-- @tity add here create DB sql script
