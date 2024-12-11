6e07f918ef34380d6020ea8217ece74144760bab
Tilen Komel <me@komelt.dev>
2024-12-11 18:21:53 +0100

    Fixing Titys bugs

--- db/docker-entrypoint-initdb.d/01.sql
+++ db/docker-entrypoint-initdb.d/01.sql
@@ -1,63 +1,33 @@
+CREATE TABLE user (
+  'id' int(11) PRIMARY KEY,
+  'username' varchar(255) DEFAULT NULL,
+  'password' varchar(255) DEFAULT NULL,
+  'email' varchar(255) DEFAULT NULL,
+  'name' varchar(255) DEFAULT NULL,
+  'phone' varchar(255) DEFAULT NULL,
+  'role' int(11) DEFAULT NULL,
+  'disabled' boolean DEFAULT TRUE
+);
 
+CREATE TABLE event (
+  'id' int(11) PRIMARY KEY,
+  'id_user' int(11) DEFAULT NULL,
+  'name' varchar(255) DEFAULT NULL,
+  'organisation' varchar(255) DEFAULT NULL,
+  'artist_name' varchar(255) DEFAULT NULL,
+  'date_from' date DEFAULT NULL,
+  'date_to' date DEFAULT NULL,
+  'loc_x' double DEFAULT NULL,
+  'loc_y' double DEFAULT NULL,
+  'time' time DEFAULT NULL,
+  'age_lim' int(11) DEFAULT NULL,
+  'description' text DEFAULT NULL,
+  'price' decimal(10,0) DEFAULT NULL,
+  'type' int(11) DEFAULT NULL,
+  'link' text DEFAULT NULL,
+  'online' tinyint(1) DEFAULT NULL
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `kd_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `event`
---
-
-CREATE TABLE `event` (
-  `id_event` int(11) NOT NULL,
-  `id_user` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `organisation` varchar(255) DEFAULT NULL,
-  `artist_name` varchar(255) DEFAULT NULL,
-  `date_from` date DEFAULT NULL,
-  `date_to` date DEFAULT NULL,
-  `loc_x` double DEFAULT NULL,
-  `loc_y` double DEFAULT NULL,
-  `time` time DEFAULT NULL,
-  `age_lim` int(11) DEFAULT NULL,
-  `description` text DEFAULT NULL,
-  `price` decimal(10,0) DEFAULT NULL,
-  `type` int(11) DEFAULT NULL,
-  `link` text DEFAULT NULL,
-  `online` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_slovenian_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `event`
---
-ALTER TABLE `event`
-  ADD PRIMARY KEY (`id_event`),
-  ADD UNIQUE KEY `link` (`link`) USING HASH,
-  ADD KEY `id_user` (`id_user`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `event`
---
-ALTER TABLE `event`
-  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE 'event'
+  ADD CONSTRAINT 'event_ibfk_1' FOREIGN KEY ('id_user') REFERENCES 'user' ('id') ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
