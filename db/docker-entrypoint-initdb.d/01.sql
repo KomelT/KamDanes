4e6ff16fdae4530f981eebaa46ea3935402ba225
Tilen Komel <me@komelt.dev>
2024-12-11 19:10:06 +0100

    Add autoincrement

--- db/docker-entrypoint-initdb.d/01.sql
+++ db/docker-entrypoint-initdb.d/01.sql
@@ -1,33 +1,35 @@
 CREATE TABLE user (
-  'id' int(11) PRIMARY KEY,
-  'username' varchar(255) DEFAULT NULL,
-  'password' varchar(255) DEFAULT NULL,
-  'email' varchar(255) DEFAULT NULL,
-  'name' varchar(255) DEFAULT NULL,
-  'phone' varchar(255) DEFAULT NULL,
-  'role' int(11) DEFAULT NULL,
-  'disabled' boolean DEFAULT TRUE
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `role` int DEFAULT NULL,
+  `disabled` boolean DEFAULT TRUE,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE event (
-  'id' int(11) PRIMARY KEY,
-  'id_user' int(11) DEFAULT NULL,
-  'name' varchar(255) DEFAULT NULL,
-  'organisation' varchar(255) DEFAULT NULL,
-  'artist_name' varchar(255) DEFAULT NULL,
-  'date_from' date DEFAULT NULL,
-  'date_to' date DEFAULT NULL,
-  'loc_x' double DEFAULT NULL,
-  'loc_y' double DEFAULT NULL,
-  'time' time DEFAULT NULL,
-  'age_lim' int(11) DEFAULT NULL,
-  'description' text DEFAULT NULL,
-  'price' decimal(10,0) DEFAULT NULL,
-  'type' int(11) DEFAULT NULL,
-  'link' text DEFAULT NULL,
-  'online' tinyint(1) DEFAULT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_user` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `organisation` varchar(255) DEFAULT NULL,
+  `artist_name` varchar(255) DEFAULT NULL,
+  `date_from` date DEFAULT NULL,
+  `date_to` date DEFAULT NULL,
+  `loc_x` double DEFAULT NULL,
+  `loc_y` double DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `age_lim` int DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `type` int DEFAULT NULL,
+  `link` text DEFAULT NULL,
+  `online` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-ALTER TABLE 'event'
-  ADD CONSTRAINT 'event_ibfk_1' FOREIGN KEY ('id_user') REFERENCES 'user' ('id') ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
