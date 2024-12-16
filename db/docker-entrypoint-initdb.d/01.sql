dfb48c73df5fc382628086299503e0447955ea57
Tit <tit.sober33@gmail.com>
2024-12-16 11:48:28 +0100

    Dodan user za UL scraper

--- db/docker-entrypoint-initdb.d/01.sql
+++ db/docker-entrypoint-initdb.d/01.sql
@@ -32,4 +32,6 @@ CREATE TABLE event (
 
 ALTER TABLE `event`
   ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+/*Tukaj se bodo dodajali scraperji. Vsakič ko se nov naredi mora dobiti svoj id*/
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `name`, `phone`, `role`, `disabled`) VALUES (100,'UL-scraper','1234','polz@fri.uni-lj.si','UniverzaVLjubljaniScraper', 113, 69, 0);
 COMMIT;
